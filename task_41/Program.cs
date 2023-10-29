5520323f527441f92af064989841a74b8f26057c
Alexej Ananin <vicont100@yandex.ru>
2023-10-30 02:05:23 +0300

    добавил задачу 41

---
+++ task_41/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+int[] EnterNumbers()
+{
+    Console.WriteLine("Введите произвольное количество чисел:");
+int[] M = Array.ConvertAll(Console.ReadLine()!.Split(", "), int.Parse);
+
+return M;
+}
+
+int CountNumbers(int[] M){
+int count = 0;
+for (int i = 0; i < M.Length; i++)
+{
+    if (M[i] > 0)
+    {
+        count++;
+    }
+}
+return count;
+}
+
+Console.Write($"Количество чисел больше нуля: {CountNumbers(EnterNumbers())}");
---
+++ task_43/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double[] DataInput(){
+    Console.WriteLine("Введите произвольные коэффициенты уравнений прямых b1, k1, b2, k2 в заданном формате:");
+    double[] coord = Array.ConvertAll(Console.ReadLine()!.Split(", "), double.Parse);
+
+    return coord;
+}
+
+void WriteEquations(double[] coord){
+    string sign = "+", sign2 = "+";
+    if (coord[0] < 0)
+    {
+        coord[0] = -coord[0];
+        sign = "-";
+    }
+     if (coord[2] < 0)
+    {
+        coord[2] = -coord[2];
+        sign2 = "-";
+    }
+    Console.WriteLine($"Уравнения: y = {coord[1]}x {sign} {coord[0]}; y = {coord[3]}x {sign2} {coord[2]}");
+}
+
+void Calculations(double[] coord, out double x, out double y){
+
+     x = Math.Round((coord[2] - coord[0]) / (coord[1] - coord[3]), 1);
+     y = Math.Round((coord[1]*x + coord[0]), 1);
+}
+
+double x = 0, y = 0;
+double[] coord = DataInput();
+WriteEquations(coord);
+Calculations(coord, out x, out y);
+Console.WriteLine($"Точка пересечения ({x}; {y})");
